bb9603d63219de73259d09571e347a2c3e9f58d3
dita-izaty <you@example.com>
2022-01-30 01:14:23 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Card from './pages/Card';
 import Home from './pages/Home';
 import Couple from './pages/Couple';
 import Event from './pages/Event';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
-    <Router>
       <div className='min-h-screen overflow-x-auto bg-slate-900'>
-        <Routes>
-            <Route path="/" element={<Card />} />
-        </Routes>
+        <Card />
         <Home />
         <Couple /> 
         <Event />
         <Footer />
-      </div>  
-    </Router> 
+      </div>    
   );
 }
 
--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import profile from '../images/couple.png'
 import logodir from '../images/logodir.png'
 import meant2be from '../audio/meant2be.mp3'
 
 function Card() {
-    let navigate = useNavigate();
     return (
         <div className='min-h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500 py-20 px-3'>
             <div className='flex flex-col justify-center max-w-xs mx-auto bg-gradient-to-r from-borahe to-fuchsia-200 shadow-xl rounded-xl py-12 px-12 '>
@@ -23,14 +21,7 @@ function Card() {
                     <div className='flex align-center just-center mt-3'></div>
                     <p className='text-xs xs:text-base bg-clip-text opacity-75 bg-gradient-to-r from-pink-200 to-brown-200 font-medium italic pt-2 pb-4 px-5 w-auto inline-block border-b-2'>
                         Minggu, 6 Februari 2022
-                    </p>
-
-                    <div className='flex align-center just-center mt-6'></div>
-                    <button onClick={() => {
-                        navigate("/home");}} 
-                        class= "bg-indigo-900 shadow-lg shadow-indigo-500/50 p-2 rounded-full font-normal text-white text-xs hover:bg-indigo-500 transition duration-300 ease-in-out animate-bounce">
-                        Open Invitation 💌
-                    </button>
+                    </p>                    
                 </div>              
             </div>
             
@@ -47,6 +38,16 @@ function Card() {
                     </p>
                 </footer>
 
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down-circle mx-auto mt-2 text-pink-800 animate-bounce" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <circle cx="12" cy="12" r="9" />
+                    <line x1="8" y1="12" x2="12" y2="16" />
+                    <line x1="12" y1="8" x2="12" y2="16" />
+                    <line x1="16" y1="12" x2="12" y2="16" />
+                </svg>
+                <p className='text-neutral-800 opacity-90 '>
+                    Scroll Down
+                </p>
             </div>
         </div>
     )
