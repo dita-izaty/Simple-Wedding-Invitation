d823b31d5c13fc42d0f9bd90b51f4a2f692d3f80
dita-izaty <you@example.com>
2022-01-30 07:47:07 +0700

    commit

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -38,7 +38,7 @@ function Card() {
                     </p>
                 </footer>
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down-circle mx-auto mt-2 text-pink-800 animate-bounce" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fd0061" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-down-circle mx-auto mt-2 animate-bounce" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="12" cy="12" r="9" />
                     <line x1="8" y1="12" x2="12" y2="16" />
